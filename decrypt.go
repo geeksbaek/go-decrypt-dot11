d4be1d07fd58d77132723e728b6bc2935ec4b7c3
Jongyeol Baek <geeksbaek@gmail.com>
2016-06-07 15:29:00 +0900

    backup

--- decrypt.go
+++ decrypt.go
@@ -5,27 +5,32 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"hash/crc32"
+	"os"
+	"regexp"
 )
 
 type ProtoType uint8
 
 const (
-	ProtoWEP  ProtoType = 1
-	ProtoWPA  ProtoType = 2
-	ProtoWPA2 ProtoType = 3
+	ProtoWEP ProtoType = iota
+	ProtoWPA
+	ProtoWPA2
 )
 
 type CipherType uint8
 
 const (
-	CipherTKIP CipherType = 1
-	CipherAES  CipherType = 2
+	CipherTKIP CipherType = iota
+	CipherAES
 )
 
 type Data struct {
-	Proto         ProtoType
-	Cipher        CipherType
+	Proto  ProtoType
+	Cipher CipherType
+
 	Key           []byte
+	OriginData    []byte
 	EncryptedData []byte
 	DecryptedData []byte
 }
@@ -50,11 +55,7 @@ func Decrpyt(input *Data) (*Data, error) {
 	// 	}
 	// }
 
-	if data, err := decryptWEP(input); err == nil {
-		return data, err
-	}
-
-	return nil, errors.New("All decryption attempt fails.")
+	return decryptWEP(input)
 }
 
 func decryptWEP(input *Data) (*Data, error) {
@@ -63,32 +64,49 @@ func decryptWEP(input *Data) (*Data, error) {
 	// case CipherAES:
 	// }
 
-	iv, _, ed, _, _ := parseWEP(input.EncryptedData)
+	if len(input.OriginData) < 12 {
+		return nil, errors.New("Too short.")
+	}
+
+	iv := input.OriginData[:3] // Initialization Vector
+	// keyIndex := uint8(d.OriginData[3])     // Key Index
+	input.EncryptedData = input.OriginData[4 : len(input.OriginData)-4] // Encrypted data
 
 	keyWithIv := append(iv, input.Key...)
+
 	keyStream, err := rc4.NewCipher(keyWithIv)
 	if err != nil {
 		return nil, errors.New("Key Stream Create Failed")
 	}
 
-	input.DecryptedData = make([]byte, len(ed))
-	keyStream.XORKeyStream(input.DecryptedData, ed)
+	input.DecryptedData = make([]byte, len(input.EncryptedData))
+	keyStream.XORKeyStream(input.DecryptedData, input.EncryptedData)
 
-	fmt.Println(string(input.DecryptedData))
+	dec := input.DecryptedData[:len(input.DecryptedData)-4]
+	icv := input.DecryptedData[len(input.DecryptedData)-4:]
+
+	h := crc32.NewIEEE()
+	_, err = h.Write(dec)
+	if err != nil {
+		return nil, errors.New("CRC32 Write Failed")
+	}
+
+	if h.Sum32() == binary.LittleEndian.Uint32(icv) {
+		fmt.Println("!!!")
+		fmt.Println(string(dec))
+		os.Exit(0)
+	} else if h.Sum32() == binary.BigEndian.Uint32(icv) {
+		fmt.Println("!!!")
+		fmt.Println(string(dec))
+		os.Exit(0)
+	}
+
+	fmt.Println(string(regexp.MustCompile(`\W`).ReplaceAll(dec, []byte("."))))
 
 	// fmt.Println("iv:", iv, " ki:", ki, " icv:", icv, " fcs:", fcs)
 	// fmt.Println("body length:", len(ed))
 
-	return nil, errors.New("Decrypt Failed")
-}
-
-func parseWEP(data []byte) ([]byte, []byte, []byte, []byte, uint32) {
-	iv := data[:3]                                     // Initialization Vector
-	ki := data[3:4]                                    // Key Index
-	ed := data[4 : len(data)-8]                        // Encrypted data
-	icv := data[len(data)-8 : len(data)-4]             // WEP ICV
-	fcs := binary.BigEndian.Uint32(data[len(data)-4:]) // Frame check sequence
-	return iv, ki, ed, icv, fcs
+	return input, nil
 }
 
 func decryptWPA(input *Data) (*Data, error) {
--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 )
 
-var (
-	radioTap      layers.RadioTap
-	dot11         layers.Dot11
-	dot11WEPLayer layers.Dot11WEP
-)
+// var (
+// 	radioTap     layers.RadioTap
+// 	dot11        layers.Dot11
+// 	dot11Data    layers.Dot11Data
+// 	dot11DataQos layers.Dot11DataQOS
+// )
 
 var (
 	WEPTestkey = []byte("12345")
@@ -26,43 +26,71 @@ func main() {
 	handle := getPcapHandle(device.Name)
 	defer handle.Close()
 
+	// Decode a packet
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		parser := gopacket.NewDecodingLayerParser(
-			layers.LayerTypeRadioTap,
-			&radioTap,
-			&dot11,
-			&dot11WEPLayer,
-		)
-		foundLayerTypes := []gopacket.LayerType{}
-
-		err := parser.DecodeLayers(packet.Data(), &foundLayerTypes)
-		if err != nil {
-			continue
-		}
-
-		for _, layerType := range foundLayerTypes {
-			switch layerType {
-
-			case layers.LayerTypeRadioTap:
-				log.Println("RadioTap Length :", len(radioTap.Contents))
-				// dump(radioTap.LayerContents())
-
-			case layers.LayerTypeDot11:
-				log.Println("Dot11 Length :", len(dot11.Contents))
-				// dump(dot11.LayerContents())
-
-			case layers.LayerTypeDot11WEP:
-				log.Println("Dot11WEP Length :", len(dot11WEPLayer.Contents))
-				// dump(dot11WEPLayer.LayerContents())
-				Decrpyt(&Data{
-					EncryptedData: dot11WEPLayer.LayerContents(),
-					Key:           WEPTestkey,
-					Proto:         ProtoWEP,
-				})
+		// Get the TCP layer from this packet
+		if w := packet.Layer(layers.LayerTypeDot11); w != nil {
+			_, err := Decrpyt(&Data{
+				OriginData: w.LayerPayload(),
+				Key:        WEPTestkey,
+				Proto:      ProtoWEP,
+			})
+			if err != nil {
+				log.Println(err)
 			}
 		}
-
-		fmt.Println()
 	}
+
+	// packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	// for packet := range packetSource.Packets() {
+	// 	parser := gopacket.NewDecodingLayerParser(
+	// 		layers.LayerTypeRadioTap,
+	// 		&radioTap,
+	// 		&dot11,
+	// 		&dot11Data,
+	// 		&dot11DataQos,
+	// 	)
+	// 	foundLayerTypes := []gopacket.LayerType{}
+
+	// 	err := parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+	// 	if err != nil {
+	// 		continue
+	// 	}
+
+	// 	for _, layerType := range foundLayerTypes {
+	// 		switch layerType {
+
+	// 		case layers.LayerTypeRadioTap:
+	// 			// log.Println("RadioTap Length :", len(radioTap.Contents))
+	// 			// dump(radioTap.LayerContents())
+
+	// 		case layers.LayerTypeDot11:
+	// 			// log.Println("Dot11 Length :", len(dot11.Contents))
+	// 			// dump(dot11.LayerContents())
+
+	// 		case layers.LayerTypeDot11Data:
+	// 			_, err := Decrpyt(&Data{
+	// 				OriginData: dot11Data.LayerContents(),
+	// 				Key:        WEPTestkey,
+	// 				Proto:      ProtoWEP,
+	// 			})
+	// 			if err != nil {
+	// 				log.Println(err)
+	// 			}
+
+	// 		case layers.LayerTypeDot11DataQOSData:
+	// 			// log.Println("Dot11WEP Length :", len(dot11WEPLayer.Contents))
+	// 			// dump(dot11WEPLayer.LayerContents())
+	// 			_, err := Decrpyt(&Data{
+	// 				OriginData: dot11DataQos.LayerContents(),
+	// 				Key:        WEPTestkey,
+	// 				Proto:      ProtoWEP,
+	// 			})
+	// 			if err != nil {
+	// 				log.Println(err)
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
